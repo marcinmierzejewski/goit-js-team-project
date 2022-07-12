8f03ca902a5834d7ddc0eb8ea4e4ed8e27db996b
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-07-12 20:02:45 +0200

    added search button, and bug fix

--- src/js/fetchEvents.js
+++ src/js/fetchEvents.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 const axios = require('axios');
 
 //Function for HTTP requests - used axios library,
-export const fetchEvents = async () => {
+export const fetchEvents = async (keyword, countryCode, page) => {
   const API_KEY = 'A5eAX0q8IljtAC67wdXuGh2kSb6ZRaVP';
   const params = new URLSearchParams({
-    keyword: "basketball",
-    countryCode: 'US',
+    keyword: keyword,
+    countryCode: countryCode,
+    page: page,
     sort: 'name,asc',
   });
     const response = await axios.get(
