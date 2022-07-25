338cd19408bf40a1df926144bed79ff0ca90e58b
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-07-25 17:02:42 +0200

    fix some bugs

--- src/index.js
+++ src/index.js
@@ -70,7 +70,9 @@ const searchEvents = async () => {
 
     totalPage = events.page.totalPages;
     if (totalPage != 0){
-      Notify.success(`We found ${events.page.totalElements} events.`);
+      if(currentPage === 1){
+        Notify.success(`We found ${events.page.totalElements} events.`);
+      }      
       renderEvents(events._embedded.events);
       createPagination(totalPage, currentPage);
       hideLoading();
@@ -91,13 +93,15 @@ const searchEvents = async () => {
 function renderEvents(data) {
   const markup = data
     .map(
-      ({ id, name, dates, images, _embedded, priceRanges }) =>
+      ({ id, name, dates, images, _embedded}) =>
         `
     <div class="events__wrapper">
       <img data-id=${id}
         class="events__image"
-        src=${images.filter(i => i.ratio === '4_3').map(i => `${i.url}`)}
-
+        src=${images
+          .filter(i => i.ratio === '4_3')
+          .map(i => `${i.url}`)
+          .join(' ')}
 
         loading="lazy"
         >
@@ -149,7 +153,6 @@ function renderEventsById(dataId) {
             loading="lazy"
         >
 
-
       <div class="modal__photo">
         <img
           class="photo"
@@ -169,7 +172,6 @@ function renderEventsById(dataId) {
             <p class="modal__text info"> ${info ? info : name} </p>
           </li>
 
-
       <li class="modal__item">
               <h6 class="modal__h6">when</h6>
               <p class="modal__text">${dates.start.localDate} </p>
@@ -177,7 +179,6 @@ function renderEventsById(dataId) {
                                   ${dates.timezone ? `(${dates.timezone})` : ''}
               </p>
             </li>
-
 
       <li class="modal__item">
               <h6 class="modal__h6">where</h6>
